18d7bea65c4185547694b340e66bac075dec3a5a
MJ <mjn220@gmail.com>
2020-12-19 01:44:08 +0330

    Contact model and view added
    در این کامیت جدید کلاس مدل کانتکت
    و همچنین ویو کانتکت ایجاد شده است

---
+++ AJBC/Models/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AJBC.Models
+{
+    public class Contact
+    {
+        [Key]
+        public int ContactId { get; set; }
+
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Message { get; set; }
+    }
+}
