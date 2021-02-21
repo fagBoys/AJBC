db11dc9b8a1682e39569a2745976ba9be8dfdb18
unknown <mjn220@gmail.com>
2021-02-21 22:38:31 +0330

    Change email sending code
    در این کامیت جدید کد های ارسال ایمیل به روز شده و ایمیل کاربر ست شده است
    همچنین شماره تلفن ها موجود به ویرایش شده اند

--- AJBC/Controllers/HomeController.cs
+++ AJBC/Controllers/HomeController.cs
@@ -23,6 +23,7 @@ namespace AJBC.Controllers
         private IHostingEnvironment _environment;
         private readonly ILogger<HomeController> _logger;
 
+
         public HomeController(ILogger<HomeController> logger, IHostingEnvironment environment)
         {
             _environment = environment;
@@ -66,10 +67,10 @@ namespace AJBC.Controllers
                 ///////    Send Email   mail:ajbbuilding49@gmail.com pass:Awedxzs09Am  ///////
                 MimeMessage message = new MimeMessage();
 
-                MailboxAddress from = new MailboxAddress("AJ Building ", "theajcontractors@outlook.com");
+                MailboxAddress from = new MailboxAddress("AJ Building ", "ajbbuilding49@gmail.com");
                 message.From.Add(from);
 
-                MailboxAddress to = new MailboxAddress("AJ Building", "theajcontractors@outlook.com");
+                MailboxAddress to = new MailboxAddress("AJ Building", "mjnsoftzerone@outlook.com");
                 message.To.Add(to);
 
                 message.Subject = " Contact";
@@ -116,7 +117,7 @@ namespace AJBC.Controllers
 
                 MimeMessage message2 = new MimeMessage();
 
-                MailboxAddress from2 = new MailboxAddress("AJ Building", "theajcontractors@outlook.com");
+                MailboxAddress from2 = new MailboxAddress("AJ Building", "ajbbuilding49@gmail.com");
                 message2.From.Add(from2);
 
                 MailboxAddress to2 = new MailboxAddress(Contact.Firstname + Contact.Lastname + " " + Contact.Subject, Contact.Email);
@@ -184,7 +185,7 @@ namespace AJBC.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Contact(Contact? contact, string? Firstname, string? Lastname, string? Emailaddress, string? Subject, string? Message)
+        public async Task<IActionResult> Contact(ContactViewModel? contact, string? Firstname, string? Lastname, string? Emailaddress, string? Subject, string? Message)
         {
             ViewData["Title"] = "Contact";
 
@@ -192,12 +193,7 @@ namespace AJBC.Controllers
             AJBCContext context = new AJBCContext();
             if (Firstname != null & Lastname != null & Emailaddress != null & Subject != null & Message != null)
             {
-                if (!ModelState.IsValid)
-                {
-                    return new RedirectResult("/Home/Index");
-                }
-                else
-                {
+
                     var Contact1 = new Contact { Firstname = Firstname, Lastname = Lastname, Subject = Subject, Email = Emailaddress, Message = Message };
                     context.Contact.Add(Contact1);
                     context.SaveChanges();
@@ -205,7 +201,7 @@ namespace AJBC.Controllers
                     ///////    Send Email   mail:ajbbuilding49@gmail.com pass:Awedxzs09Am  ///////
                     MimeMessage message = new MimeMessage();
 
-                    MailboxAddress from = new MailboxAddress("AJ Building ", "theajcontractors@outlook.com");
+                    MailboxAddress from = new MailboxAddress("AJ Building ", "ajbbuilding49@gmail.com");
                     message.From.Add(from);
 
                     MailboxAddress to = new MailboxAddress("AJ Building", "theajcontractors@outlook.com");
@@ -221,8 +217,8 @@ namespace AJBC.Controllers
 
                     var mybody = @System.IO.File.ReadAllText(_environment.WebRootPath + @"\Email\emailbody-contact.html");
 
-                    mybody = mybody.Replace("Value00", Contact1.Firstname + Contact1.Lastname);
-                    mybody = mybody.Replace("Value01", Contact1.Firstname + Contact1.Lastname);
+                    mybody = mybody.Replace("Value00", Contact1.Firstname + " " + Contact1.Lastname);
+                    mybody = mybody.Replace("Value01", Contact1.Firstname + " " + Contact1.Lastname);
                     mybody = mybody.Replace("Value02", Contact1.Email);
                     //mybody = mybody.Replace("Value03", Contact1.PhoneNumber);
                     mybody = mybody.Replace("Value04", Contact1.Subject);
@@ -255,7 +251,7 @@ namespace AJBC.Controllers
 
                     MimeMessage message2 = new MimeMessage();
 
-                    MailboxAddress from2 = new MailboxAddress("AJ Building", "theajcontractors@outlook.com");
+                    MailboxAddress from2 = new MailboxAddress("AJ Building", "ajbbuilding49@gmail.com");
                     message2.From.Add(from2);
 
                     MailboxAddress to2 = new MailboxAddress(Contact1.Firstname + Contact1.Lastname + " " + Contact1.Subject, Contact1.Email);
@@ -302,19 +298,117 @@ namespace AJBC.Controllers
                     return new RedirectResult("/Home/Index");
 
 
-                }
+
 
             }
             else
             {
                 if (!ModelState.IsValid)
                 {
-                    return View(!ModelState.IsValid ? contact : new Contact());
+                    return View(!ModelState.IsValid ? contact : new ContactViewModel());
                 }
                 else
                 {
-                    context.Contact.Add(contact);
+                    context.Contact.Add(new Contact { Firstname = contact.Firstname, Lastname = contact.Lastname, Subject = contact.Subject, Email = contact.Email, Message = contact.Message });
                     context.SaveChanges();
+
+                    ///////    Send Email   mail:ajbbuilding49@gmail.com pass:Awedxzs09Am  ///////
+                    MimeMessage message = new MimeMessage();
+
+                    MailboxAddress from = new MailboxAddress("AJ Building ", "ajbbuilding49@gmail.com");
+                    message.From.Add(from);
+
+                    MailboxAddress to = new MailboxAddress("AJ Building", "theajcontractors@outlook.com");
+                    message.To.Add(to);
+
+                    message.Subject = " Contact";
+
+                    BodyBuilder bodyBuilder = new BodyBuilder();
+                    var usericfile = System.IO.File.OpenRead(_environment.WebRootPath + @"\Email\newuser.png");
+                    MemoryStream newms = new MemoryStream();
+                    await usericfile.CopyToAsync(newms);
+
+
+                    var mybody = @System.IO.File.ReadAllText(_environment.WebRootPath + @"\Email\emailbody-contact.html");
+
+                    mybody = mybody.Replace("Value00", contact.Firstname + " " + contact.Lastname);
+                    mybody = mybody.Replace("Value01", contact.Firstname + " " + contact.Lastname);
+                    mybody = mybody.Replace("Value02", contact.Email);
+                    //mybody = mybody.Replace("Value03", Contact1.PhoneNumber);
+                    mybody = mybody.Replace("Value04", contact.Subject);
+                    mybody = mybody.Replace("Value05", contact.Message);
+
+
+
+                    bodyBuilder.HtmlBody = mybody;
+
+                    var usericon = bodyBuilder.LinkedResources.Add(_environment.WebRootPath + @"/Email/newuser.png");
+                    usericon.ContentId = MimeUtils.GenerateMessageId();
+
+                    bodyBuilder.HtmlBody = bodyBuilder.HtmlBody.Replace("{", "{{");
+                    bodyBuilder.HtmlBody = bodyBuilder.HtmlBody.Replace("}", "}}");
+                    bodyBuilder.HtmlBody = bodyBuilder.HtmlBody.Replace("{{0}}", "{0}");
+
+                    bodyBuilder.HtmlBody = string.Format(bodyBuilder.HtmlBody, usericon.ContentId);
+
+                    message.Body = bodyBuilder.ToMessageBody();
+
+
+                    SmtpClient client = new SmtpClient();
+                    client.Connect("smtp.gmail.com", 465, true);
+                    client.Authenticate("ajbbuilding49@gmail.com", "Awedxzs09Am");
+
+
+                    client.Send(message);
+                    //First email
+
+
+                    MimeMessage message2 = new MimeMessage();
+
+                    MailboxAddress from2 = new MailboxAddress("AJ Building", "ajbbuilding49@gmail.com");
+                    message2.From.Add(from2);
+
+                    MailboxAddress to2 = new MailboxAddress(contact.Firstname + contact.Lastname + " " + contact.Subject, contact.Email);
+                    message2.To.Add(to2);
+
+                    message2.Subject = "Message received";
+
+
+                    BodyBuilder bobu = new BodyBuilder
+                    {
+                        HtmlBody = @System.IO.File.ReadAllText(_environment.WebRootPath + @"\Email\emailreply-contact.html")
+                    };
+
+
+
+
+                    // var logo = System.IO.File.OpenRead(_environment.WebRootPath + @"/img/logo.png");
+                    MemoryStream myms = new MemoryStream();
+                    await usericfile.CopyToAsync(myms);
+
+                    var embedlogo = bobu.LinkedResources.Add(_environment.WebRootPath + @"/img/logo.png");
+                    embedlogo.ContentId = MimeUtils.GenerateMessageId();
+
+                    bobu.HtmlBody = bobu.HtmlBody.Replace("{", "{{");
+                    bobu.HtmlBody = bobu.HtmlBody.Replace("}", "}}");
+                    bobu.HtmlBody = bobu.HtmlBody.Replace("{{0}}", "{0}");
+
+                    bobu.HtmlBody = string.Format(bobu.HtmlBody, embedlogo.ContentId);
+
+
+                    message2.Body = bobu.ToMessageBody();
+
+                    SmtpClient client2 = new SmtpClient();
+                    client2.Connect("smtp.gmail.com", 465, true);
+                    client2.Authenticate("ajbbuilding49@gmail.com", "Awedxzs09Am");
+
+
+                    client2.Send(message2);
+                    client2.Disconnect(true);
+                    client2.Dispose();
+                    ///////   End Send Email    //////////
+
+
                     return View();
                 }
             }
